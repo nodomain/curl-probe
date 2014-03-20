a6524603fcca654a72dbafce4fdfd7566dcd35ed
Fabian Fischer <ff@nodomain.cc>
2014-03-20 21:57:37 +0100

    Added a very simple locking algorithm to avoid cron overlaps Source: http://stackoverflow.com/questions/5428631/php-preventing-collision-in-cron-file-lock-safe

--- cron.php
+++ cron.php
@@ -9,94 +9,103 @@ require_once('class/class.shorturl.php');
 $currentminute = date("i");
 //$currentminute = 5;
 
-$sTest = new serverTest();
-try {
-  $db = new database($dbuser, $dbpass, $dbhost, $dbname);
-  $servers = $db->getAllProbesToCheck();
-  foreach ($servers as $server) {
-    if ($currentminute % $server['checkinterval'] == 0) {
-      print("Probing ".$server['name']);
-      $mystatus = false;
-      $sTest->setTitle($server['name']);
-      $sTest->setServer($server['url']);
-      $sTest->setHost($server['host']);
-      $sTest->setFindstring($server['findstring']);
-      $sTest->setVersion($version);
-      $sTest->setHostname($hostname);
-      $sTest->test();
+$fp = fopen("./tmp/lock.txt", "w+");
 
-      // test again to make sure we really have a problem and avoid accidental mails
-      if ($sTest->getStatus() == false) {      
-        $random = rand(1, 10);
-        print (" ... trying again in ".$random." seconds to ensure we really have a problem\n");
-        sleep($random);
+if (flock($fp, LOCK_EX | LOCK_NB)) { // do an exclusive lock
+  $sTest = new serverTest();
+  try {
+    $db      = new database($dbuser, $dbpass, $dbhost, $dbname);
+    $servers = $db->getAllProbesToCheck();
+    foreach ($servers as $server) {
+      if ($currentminute % $server['checkinterval'] == 0) {
+        print("Probing " . $server['name']);
+        $mystatus = false;
+        $sTest->setTitle($server['name']);
+        $sTest->setServer($server['url']);
+        $sTest->setHost($server['host']);
+        $sTest->setFindstring($server['findstring']);
+        $sTest->setVersion($version);
+        $sTest->setHostname($hostname);
         $sTest->test();
-      }
-      
-      if ($sTest->getStatus() == false) {      
-        print (" ... failed\n");
-        // send mail only if status in DB was true -> mail will only be sent once
-        $status = $db->getProbeStatus($server['id']);
-	if ($status == true) {
-	  // insert entry into events table
-	  $db->addProbeEvent('error', $sTest->getResult(), $server['id']);
-	  // mail it
-	  $pretext = "Hello,\nan error occured while probing \"" . $server['name']. "\" \nwith URL \"". $server['url']."\"\nfor \"". $server['findstring']."\". \n\nThe server replied as follows:\n\n";
-          $posttext = "\n\nRegards,\nyour faithful curl-probe agent\nat ".$hostname;
-          $status = $db->setProbeStatus($server['id'], 0);      
-//          print ("Error: Servertest for probe '" . $sTest->getTitle() ."' failed!\n");
-          $responsible = $db->getAllResponsible($server['id']);
-          if (isset($responsible)) {
-            foreach ($responsible as $recipient) {
-              // mail it
-              if ((strtotime($startquietmode) <= time()) && (time() <= strtotime($endquietmode))) {
-                echo "not sending mail because of quiet mode\n";
-              } else {
-                echo "sending mail\n";
-                $mailer = new sendMail($recipient['email'], $mailfrom, $replyto);
-                $mailer->send($sTest->getTitle() . " - error", $pretext . $sTest->getResult() . $posttext);
+        
+        // test again to make sure we really have a problem and avoid accidental mails
+        if ($sTest->getStatus() == false) {
+          $random = rand(1, 15);
+          print(" ... trying again in " . $random . " seconds to ensure we really have a problem\n");
+          sleep($random);
+          $sTest->test();
+        }
+        
+        if ($sTest->getStatus() == false) {
+          print(" ... failed\n");
+          // send mail only if status in DB was true -> mail will only be sent once
+          $status = $db->getProbeStatus($server['id']);
+          if ($status == true) {
+            // insert entry into events table
+            $db->addProbeEvent('error', $sTest->getResult(), $server['id']);
+            // mail it
+            $pretext     = "Hello,\nan error occured while probing \"" . $server['name'] . "\" \nwith URL \"" . $server['url'] . "\"\nfor \"" . $server['findstring'] . "\". \n\nThe server replied as follows:\n\n";
+            $posttext    = "\n\nRegards,\nyour faithful curl-probe agent\nat " . $hostname;
+            $status      = $db->setProbeStatus($server['id'], 0);
+            //          print ("Error: Servertest for probe '" . $sTest->getTitle() ."' failed!\n");
+            $responsible = $db->getAllResponsible($server['id']);
+            if (isset($responsible)) {
+              foreach ($responsible as $recipient) {
+                // mail it
+                if ((strtotime($startquietmode) <= time()) && (time() <= strtotime($endquietmode))) {
+                  echo "not sending mail because of quiet mode\n";
+                } else {
+                  echo "sending mail\n";
+                  $mailer = new sendMail($recipient['email'], $mailfrom, $replyto);
+                  $mailer->send($sTest->getTitle() . " - error", $pretext . $sTest->getResult() . $posttext);
+                }
               }
+            } else {
+              throw new Exception("No responsible person found. Please set up at least one person!");
             }
           } else {
-           throw new Exception("No responsible person found. Please set up at least one person!");
+            // nothing changed
           }
-	} else {
-	  // nothing changed
-	}  
-      } else {
-        $status = $db->getProbeStatus($server['id']);
-        print (" ... ok\n");
-	if ($status == false) {
-	  // insert entry into events table
-	  $db->addProbeEvent('ok', '', $server['id']);
-	  $logtime = $db->getProbeStatusLogtime($server['id']);
-	  // mail it
-	  $pretext = "Hello,\nthe probe \"" . $server['name']. "\" with URL \"". $server['url']."\", probing for \"". $server['findstring']."\", has recovered.";
-	  $pretext = $pretext."\nIt had been on error since ".date($timeformat, strtotime($logtime))." (".round(abs(time() - strtotime($logtime)) / 60,1)." minute(s)).";
-          $posttext = "\n\nRegards,\nyour faithful curl-probe agent\nat ".$hostname;
-          $status = $db->setProbeStatus($server['id'], 1);
-//          print ("Error: Servertest for probe '" . $sTest->getTitle() ."' failed!\n");
-          $responsible = $db->getAllResponsible($server['id']);
-          if (isset($responsible)) {
-            foreach ($responsible as $recipient) {
-              // mail it
-              if ((strtotime($startquietmode) <= time()) && (time() <= strtotime($endquietmode))) {
-                echo "not sending mail because of quiet mode\n";
-              } else {
-                echo "sending mail\n";
-                $mailer = new sendMail($recipient['email'], $mailfrom, $replyto);
-                $mailer->send($sTest->getTitle() . " - recovered", $pretext . $posttext);
+        } else {
+          $status = $db->getProbeStatus($server['id']);
+          print(" ... ok\n");
+          if ($status == false) {
+            // insert entry into events table
+            $db->addProbeEvent('ok', '', $server['id']);
+            $logtime     = $db->getProbeStatusLogtime($server['id']);
+            // mail it
+            $pretext     = "Hello,\nthe probe \"" . $server['name'] . "\" with URL \"" . $server['url'] . "\", probing for \"" . $server['findstring'] . "\", has recovered.";
+            $pretext     = $pretext . "\nIt had been on error since " . date($timeformat, strtotime($logtime)) . " (" . round(abs(time() - strtotime($logtime)) / 60, 1) . " minute(s)).";
+            $posttext    = "\n\nRegards,\nyour faithful curl-probe agent\nat " . $hostname;
+            $status      = $db->setProbeStatus($server['id'], 1);
+            //          print ("Error: Servertest for probe '" . $sTest->getTitle() ."' failed!\n");
+            $responsible = $db->getAllResponsible($server['id']);
+            if (isset($responsible)) {
+              foreach ($responsible as $recipient) {
+                // mail it
+                if ((strtotime($startquietmode) <= time()) && (time() <= strtotime($endquietmode))) {
+                  echo "not sending mail because of quiet mode\n";
+                } else {
+                  echo "sending mail\n";
+                  $mailer = new sendMail($recipient['email'], $mailfrom, $replyto);
+                  $mailer->send($sTest->getTitle() . " - recovered", $pretext . $posttext);
+                }
               }
+            } else {
+              throw new Exception("No responsible person found. Please set up at least one person!");
             }
-          } else {
-            throw new Exception("No responsible person found. Please set up at least one person!");
           }
         }
       }
     }
   }
-} catch (Exception $ex) {
-  // send mail    
-  print("Exception occured for probe '" . $sTest->getTitle() . "': " . $ex->getMessage()."\n");
+  catch (Exception $ex) {
+    // send mail
+    print("Exception occured for probe '" . $sTest->getTitle() . "': " . $ex->getMessage() . "\n");
+  }
+  flock($fp, LOCK_UN); // release the lock
+} else {
+  print("Couldn't get the lock!");
 }
+fclose($fp);
 ?>
